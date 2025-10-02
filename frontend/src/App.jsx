852f3d524c193c3600ece0645808832caf1d5f67
Kahier <78684070+Kahier@users.noreply.github.com>
2025-10-02 13:46:02 +0300

    Initial project setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from "react";
+
+function App() {
+
+  return (
+    <div className="app">
+      <h1>Welcome to Renart</h1>
+    </div>
+  );
+}
+
+export default App;
